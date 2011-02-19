0a9901c28332c9b5a735514a91aef64d5580b141
Marek Defeciński <marekdef@gmail.com>
2011-02-19 10:04:26 +0100

    Added smssender project skeleton

---
+++ smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
@@ -0,0 +1,13 @@
+package net.retsat1.starlab.smssender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduledSmsList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
