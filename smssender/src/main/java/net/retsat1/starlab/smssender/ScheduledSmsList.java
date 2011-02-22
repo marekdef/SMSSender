da2c22a76c3c0a690208de99e638991b1f1aebe7
Marek Defeciński <marekdef@gmail.com>
2011-02-22 23:27:22 +0100

    Forgot about R

--- smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
+++ smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
@@ -3,6 +3,8 @@ package net.retsat1.starlab.smssender;
 import android.app.Activity;
 import android.os.Bundle;
 
+import net.retsat1.starlab.smssender.R;
+
 public class ScheduledSmsList extends Activity {
     /** Called when the activity is first created. */
     @Override
