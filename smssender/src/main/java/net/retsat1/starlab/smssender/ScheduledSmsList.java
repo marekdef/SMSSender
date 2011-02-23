a8418c4c1559d80aa35b2b44ee30485fddad7b0e
Mariusz Saramak <mariopce@gmail.com>
2011-02-23 22:58:28 +0100

    some small changes

--- smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
+++ smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
@@ -2,12 +2,40 @@ package net.retsat1.starlab.smssender;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ScheduledSmsList extends Activity {
-    /** Called when the activity is first created. */
+    private static final String TAG = "ScheduledSmsList";
+
+    Button button; 
+    LinearLayout linearLayout;
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Debug.waitForDebugger();
         setContentView(R.layout.main);
+        linearLayout = (LinearLayout) findViewById(R.id.topLayaout);
+        Log.d(TAG, "wiadomość widdsdsfsfsfsfsfadomosć ");
+        button = new Button(this);
+        
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.d(TAG, "click1");
+				
+				
+				Log.d(TAG, "click2");
+				Log.d(TAG, "click3");
+			}
+		});
+        linearLayout.addView(button);
+        
     }
 }
