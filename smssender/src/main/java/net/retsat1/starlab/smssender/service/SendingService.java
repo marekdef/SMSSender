4371708e15235e5fd37010c25fda7d0de78effb8
Marek Defeciński <marekdef@gmail.com>
2011-03-22 23:03:20 +0100

    Added service class

---
+++ smssender/src/main/java/net/retsat1/starlab/smssender/service/SendingService.java
@@ -0,0 +1,38 @@
+package net.retsat1.starlab.smssender.service;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class SendingService extends Service {
+	private static final String TAG = SendingService.class.getSimpleName();
+	private NotificationManager notificationManager;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(TAG, "SendingService.onBind()");
+		return mBinder;
+	}
+	
+	@Override
+	public void onCreate() {
+		Log.d(TAG, "SendingService.onCreate()");
+		notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		
+		super.onCreate();
+	}
+
+	private final IBinder mBinder = new Binder() {
+        @Override
+                protected boolean onTransact(int code, Parcel data, Parcel reply,
+                        int flags) throws RemoteException {
+        	Log.d(TAG, "SendingService.onTransact()");
+            return super.onTransact(code, data, reply, flags);
+        }
+    };
+}
