b9efe8b0acfefa5b5ec947b0138cf6ebf08ee43e
Marek Defeciński <marekdef@gmail.com>
2011-03-27 16:11:06 +0200

    Optimized calendar

--- smssender/src/main/java/net/retsat1/starlab/smssender/ScheduleNewSms.java
+++ smssender/src/main/java/net/retsat1/starlab/smssender/ScheduleNewSms.java
@@ -1,26 +1,16 @@
 package net.retsat1.starlab.smssender;
 
-
-
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
-
 import net.retsat1.starlab.android.timepicker.DetailedTimePicker;
-
 import net.retsat1.starlab.smssender.dto.SmsMessage;
-import net.retsat1.starlab.smssender.service.SendingService;
-import net.retsat1.starlab.android.timepicker.DetailedTimePicker;
-
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
-import android.net.NetworkInfo.DetailedState;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
@@ -33,120 +23,100 @@ import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
 
 public class ScheduleNewSms extends Activity {
-	private static final String TAG = ScheduleNewSms.class.getSimpleName();
+    private static final String TAG = ScheduleNewSms.class.getSimpleName();
 
-	private Button sendButton;
+    private Button sendButton;
 
-	private DatePicker datePicker;
-	
-	private DetailedTimePicker timePicker;
+    private DatePicker datePicker;
 
-	private AutoCompleteTextView numberEditText;
+    private DetailedTimePicker timePicker;
 
-	private EditText messageEditText;
-	
-	private PendingIntent pendingIntent;
+    private AutoCompleteTextView numberEditText;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
+    private EditText messageEditText;
 
-		setContentView(R.layout.schedule);
+    private PendingIntent pendingIntent;
 
-		datePicker = (DatePicker) findViewById(R.id.dataPicker);
-		timePicker = (DetailedTimePicker) findViewById(R.id.detailedTimePicker);
-		numberEditText = (AutoCompleteTextView) findViewById(R.id.numberEditText);
-		messageEditText = (EditText) findViewById(R.id.messageEditText);
-		sendButton = (Button) findViewById(R.id.send_button);
-		sendButton.setOnClickListener(new OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
 
-			@Override
-			public void onClick(View v) {
-				String number = numberEditText.getText().toString();
-				String message = messageEditText.getText().toString();
-				
-				sendMessage(System.currentTimeMillis(), number, message);
-			}
-		});
+        setContentView(R.layout.schedule);
 
-		setAdapterForNumberEditor();
-	}
+        datePicker = (DatePicker) findViewById(R.id.dataPicker);
+        timePicker = (DetailedTimePicker) findViewById(R.id.detailedTimePicker);
+        numberEditText = (AutoCompleteTextView) findViewById(R.id.numberEditText);
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+        sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new OnClickListener() {
 
-	private void setAdapterForNumberEditor() {
-		ContentResolver content = getContentResolver();
-		String SELECTION = ContactsContract.Contacts.HAS_PHONE_NUMBER + "='1'";
-		String[] PROJECTION = new String[] {
+            @Override
+            public void onClick(View v) {
+                String number = numberEditText.getText().toString();
+                String message = messageEditText.getText().toString();
 
-		ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME,
-				ContactsContract.Contacts.HAS_PHONE_NUMBER, };
+                sendMessage(number, message);
+            }
+        });
 
-		Cursor cursor = content.query(ContactsContract.Contacts.CONTENT_URI,
-				PROJECTION, SELECTION, null, null);
+        setAdapterForNumberEditor();
+    }
 
-		if (cursor == null) {
-			Log.w(TAG, "No contacts to display");
-		} else {
-			while (cursor.moveToNext()) {
-				String data = cursor
-						.getString(cursor
-								.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+    private void setAdapterForNumberEditor() {
+        ContentResolver content = getContentResolver();
+        String SELECTION = ContactsContract.Contacts.HAS_PHONE_NUMBER + "='1'";
+        String[] PROJECTION = new String[] {
 
-				int hasNumber = cursor
-						.getInt(cursor
-								.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+        ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER, };
 
-				Log.d(TAG, "data " + data + " hasNumber=" + hasNumber);
-			}
+        Cursor cursor = content.query(ContactsContract.Contacts.CONTENT_URI, PROJECTION, SELECTION, null, null);
 
-			String[] columns = new String[] {
-					ContactsContract.Contacts.DISPLAY_NAME,
-					ContactsContract.Contacts.HAS_PHONE_NUMBER };
-			int[] names = new int[] { R.id.row_display_name,
-					R.id.row_phone_number };
+        if (cursor == null) {
+            Log.w(TAG, "No contacts to display");
+        } else {
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
-			SimpleCursorAdapter adapterPhone = new SimpleCursorAdapter(this,
-					R.layout.phone_row_entry, cursor, columns, names);
-			startManagingCursor(cursor);
-			numberEditText.setAdapter(adapterPhone);
+                int hasNumber = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
 
-		}
+                Log.d(TAG, "data " + data + " hasNumber=" + hasNumber);
+            }
 
-	}
+            String[] columns = new String[] { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER };
+            int[] names = new int[] { R.id.row_display_name, R.id.row_phone_number };
 
-	private void sendMessage(long currentTimeMillis, String number,
-			String message) {
-		
-		Log.d(TAG, "sendMessage number " + number  + " message " + message);
-		Log.d(TAG, "year: " + datePicker.getYear());
-		Log.d(TAG, "month: " + datePicker.getMonth());
-		Log.d(TAG, "day: " + datePicker.getDayOfMonth());
-		Log.d(TAG, "hh: " + timePicker.getCurrentHour());
-		Log.d(TAG, "mm: " + timePicker.getCurrentMinute());
-		Log.d(TAG, "ss: " + timePicker.getCurrentSecond());
+            SimpleCursorAdapter adapterPhone = new SimpleCursorAdapter(this, R.layout.phone_row_entry, cursor, columns, names);
+            startManagingCursor(cursor);
+            numberEditText.setAdapter(adapterPhone);
 
-		Calendar c = GregorianCalendar.getInstance();
-		c.set(Calendar.YEAR, datePicker.getYear());
-		c.set(Calendar.MONTH, datePicker.getMonth());
-		c.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
-		
-		c.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
-		c.set(Calendar.MINUTE, timePicker.getCurrentMinute());
-		c.set(Calendar.SECOND, timePicker.getCurrentSecond());
-		c.set(Calendar.MILLISECOND, 0);
-		Log.d(TAG, " data   current " + currentTimeMillis + " c="+c.getTimeInMillis());
-		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-		alarmManager.set(AlarmManager.RTC_WAKEUP,
-				c.getTimeInMillis(), pendingIntent);
-		ContentValues values = new ContentValues();
-		 values.put(SmsMessage.MESSAGE, "ASP.NET 2.0: A Developer's Notebook");
-		   values.put(SmsMessage.RECEIVER, "0596008120");        
-		getContentResolver().insert(SmsMessage.CONTENT_URI, values );
+        }
 
-	}
+    }
+
+    private void sendMessage(String number, String message) {
+        Log.d(TAG, "sendMessage number " + number + " message " + message);
+        Log.d(TAG, "year: " + datePicker.getYear());
+        Log.d(TAG, "month: " + datePicker.getMonth());
+        Log.d(TAG, "day: " + datePicker.getDayOfMonth());
+        Log.d(TAG, "hh: " + timePicker.getCurrentHour());
+        Log.d(TAG, "mm: " + timePicker.getCurrentMinute());
+        Log.d(TAG, "ss: " + timePicker.getCurrentSecond());
+
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), timePicker.getCurrentHour(), timePicker.getCurrentMinute(),
+                timePicker.getCurrentSecond());
+
+        Log.d(TAG, "Data when" + currentTimeMillis + " c=" + calendar.getTimeInMillis());
+        
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        
+        ContentValues values = new ContentValues();
+        values.put(SmsMessage.MESSAGE, "ASP.NET 2.0: A Developer's Notebook");
+        values.put(SmsMessage.RECEIVER, "0596008120");
+        
+        getContentResolver().insert(SmsMessage.CONTENT_URI, values);
+    }
 
 }
-
-
-
-
