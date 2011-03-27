efb1eb8837a9216e91f4c92a92e1d695f7edb9ae
Mariusz Saramak <mariopce@gmail.com>
2011-03-27 12:21:38 +0200

    marge rename files

---
+++ smssender/src/main/java/net/retsat1/starlab/smssender/ScheduledSmsList.java
@@ -0,0 +1,49 @@
+package net.retsat1.starlab.smssender;
+
+import net.retsat1.starlab.smssender.ui.adapter.SmsListAdapter;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class ScheduledSmsList extends Activity {
+	private static final String TAG = ScheduledSmsList.class.getSimpleName();
+
+	private ListView smsListView;
+
+	private SmsListAdapter smsListAdapter;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.list);
+		
+		smsListView = (ListView) findViewById(R.id.smsList);
+//		smsListAdapter = new SmsListAdapter(this, R.layout.list_item);
+//		smsListAdapter.add(new SmsMessage("ToNumber", "Message"));
+//		smsListView.setAdapter(smsListAdapter);
+		
+	}
+
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.new_sms:
+			startActivity(new Intent(this, ScheduleNewSms.class));
+			return true;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+}
